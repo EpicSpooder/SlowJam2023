91732b9b1e82189cf4da93910a5324cd298d2078
Daniel Hiley <daniel.hiley@digipen.edu>
2023-11-18 13:59:58 -0800

    Changed Slime Pulsate Script

--- Assets/Scripts/SlimePulsate.cs
+++ Assets/Scripts/SlimePulsate.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 public class SlimePulsate : MonoBehaviour
 {
     public float pulsationFrequency = 1.0f;  // Adjust the frequency of pulsation
-    public float pulsationAmplitude = 0.1f;   // Adjust the amplitude of pulsation
+    public float pulsationAmplitudeX = 0.2f;  // Adjust the amplitude of pulsation along X
+    public float pulsationAmplitudeY = 0.1f; // Adjust the amplitude of pulsation along Y
     public float randomOffsetRange = 0.1f;    // Introduce randomness for variation
 
     private Vector3 initialScale;
@@ -17,11 +18,14 @@ public class SlimePulsate : MonoBehaviour
 
     void Update()
     {
-        // Calculate pulsation factor based on time and add the unique random offset
-        float pulsationFactor = Mathf.Sin((Time.time + randomOffset) * pulsationFrequency)
-                               * pulsationAmplitude;
+        // Calculate pulsation factors based on time and add the unique random offset
+        float pulsationFactorX = Mathf.Sin((Time.time + randomOffset) * pulsationFrequency)
+                                * pulsationAmplitudeX;
+        float pulsationFactorY = Mathf.Sin((Time.time + randomOffset) * pulsationFrequency)
+                                * pulsationAmplitudeY;
 
-        // Apply pulsation to the scale
-        transform.localScale = initialScale * (1.0f + pulsationFactor);
+        // Apply pulsation to the scale independently along X and Y
+        Vector3 newScale = new Vector3(1.0f + pulsationFactorX, 1.0f + pulsationFactorY, 1.0f);
+        transform.localScale = Vector3.Scale(initialScale, newScale);
     }
 }
